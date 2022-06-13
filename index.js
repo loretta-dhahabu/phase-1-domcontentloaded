ba68c05b97d98fdd9463472aa1cce33cec62191e
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-13 21:28:32 +0300

    commit tree 920a835017f127edcd46d56da69cbcaef904584d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// const event = document.addEventListener( "DOMContentLoaded", function ()
+// {
+    
+// })
+
+// document.addEventListener( "DOMContentLoaded",function eventFunction (){
+//     document.querySelector( "#text" ).textContent = "This is really cool!"
+// } );
+
+// document.addEventListener( "DOMContentLoaded", ()=>{
+//     document.querySelector( "#text" ).textContent = "This is really cool!"
+// } )
+document.addEventListener("DOMContentLoaded", function eventFunction () {
+ 
+  const textElement = document.getElementById("text");
+  textElement.textContent = "This is really cool!";
+});
